41d21eeec12e8a37417faebc004227d96999870c
tadhityanu <taufanadhitya21@gmail.com>
2024-10-15 11:00:07 +0700

    Updates

--- src/config/loadImage.js
+++
@@ -1,6 +0,0 @@
-import images from "../assets/Image";
-
- export const loadImage = async (imageKey) => {
-    const imageModule = await images[imageKey]();
-    return imageModule.default; // Assuming the images are exported as default
-};
